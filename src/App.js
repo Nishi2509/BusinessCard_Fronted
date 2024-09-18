557f6ce6877459828f0e5129fcf946911951fb2c
Nishi2509 <nishiagrawal2509@gmail.com>
2024-09-18 15:58:29 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,33 +1,71 @@
-import React, { useState } from 'react'
-import Home from './Pages/Home/Home'
-import Profile from './Pages/Profile/Profile'
-import FriendsId from "./Pages/FriendsId/FriendsId"
-import { Route, Routes } from 'react-router-dom'
-import Notification from './Pages/Notification/Notification'
-import Login from './Pages/RegisterPage/Login'
-import SignUp from './Pages/RegisterPage/SignUp'
+import React, { useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Profile from './Pages/Profile/Profile';
+import FriendsId from "./Pages/FriendsId/FriendsId";
+import Notification from './Pages/Notification/Notification';
+import Login from './Pages/RegisterPage/Login';
+import SignUp from './Pages/RegisterPage/SignUp';
+import Info from './Components/Profile/ProfileComponents/InfoProfile/Info'; // Import Info component
 
 const App = () => {
-  const [friendProfile,setFriendsProfile] =useState([])
+  const [friendProfile, setFriendsProfile] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Manage login state
+  const [userPostData, setUserPostData] = useState([]);
+  const [following, setFollowing] = useState(0);
+  const [modelDetails, setModelDetails] = useState({});
+  const [profileImg, setProfileImg] = useState(null);
+  const [name, setName] = useState('');
+  const [userName, setUserName] = useState('');
 
   return (
     <div className='App'>
       <Routes>
-        <Route path='/home' element={<Home setFriendsProfile={setFriendsProfile}/> } />
-        
-        <Route path='/profile' element={ <Profile /> } />
+        {/* If user is logged in, show Info as homepage, otherwise redirect to login */}
+        <Route 
+          path='/home' 
+          element={isLoggedIn ? (
+            <Info
+              userPostData={userPostData}
+              following={following}
+              modelDetails={modelDetails}
+              setModelDetails={setModelDetails}
+              profileImg={profileImg}
+              setProfileImg={setProfileImg}
+              name={name}
+              setName={setName}
+              userName={userName}
+              setUserName={setUserName}
+            />
+          ) : (
+            <Navigate to="/" />
+          )}
+        />
 
-        <Route path='/friendsId' element={<FriendsId friendProfile={friendProfile} />} />
+        <Route 
+          path='/profile' 
+          element={isLoggedIn ? <Profile /> : <Navigate to="/" />} 
+        />
+
+        <Route 
+          path='/friendsId' 
+          element={isLoggedIn ? <FriendsId friendProfile={friendProfile} /> : <Navigate to="/" />} 
+        />
       
-        <Route path='/notification' element={<Notification />} />
+        <Route 
+          path='/notification' 
+          element={isLoggedIn ? <Notification /> : <Navigate to="/" />} 
+        />
 
-        <Route path='/' element={<Login />} />
-
+        {/* Login and SignUp routes */}
+        <Route path='/' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
         <Route path='/signup' element={<SignUp />} />
-        
+
+        {/* Fallback redirect */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Pages/RegisterPage/Login.js
+++ src/Pages/RegisterPage/Login.js
@@ -1,106 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import {FiMail} from "react-icons/fi"
-import {RiLockPasswordLine} from "react-icons/ri"
-import "../RegisterPage/RegisterPage.css"
-import { Link, useNavigate } from 'react-router-dom'
-
+import React, { useState, useEffect } from 'react';
+import { FiMail } from "react-icons/fi";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { Link, useNavigate } from 'react-router-dom';
+import validation from './Validation'; 
 
 const Login = () => {
-    const navigate =useNavigate()
-    const [error,setError] =useState({})
-    const [submit,setSubmit] =useState(false)
-  
-    const [data,setData] =useState({
-        email:"",
-        password:"",
-    })
+    const navigate = useNavigate();
+    const [error, setError] = useState({});
+    const [submit, setSubmit] = useState(false);
 
-    const handleChange=(e)=>{
-        const newObj={...data,[e.target.name]:e.target.value}
-        setData(newObj)
-    }
- 
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+    });
 
-    const handleSignUp=(e)=>{
-        e.preventDefault()
-       setError(validationLogin(data))
-       setSubmit(true)
-    }
+    const handleChange = (e) => {
+        const newObj = { ...data, [e.target.name]: e.target.value };
+        setData(newObj);
+    };
 
-    useEffect(()=>{
-        if(Object.keys(error).length === 0 && submit){
-            navigate("/home")
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        const validationErrors = validation(data);
+        console.log('Validation Errors:', validationErrors); // Debugging
+        setError(validationErrors);
+        setSubmit(true);
+    };
+
+    useEffect(() => {
+        console.log('Effect Triggered', { error, submit }); // Debugging
+        if (Object.keys(error).length === 0 && submit) {
+            navigate("/home");
         }
-    },[error])
+    }, [error, navigate, submit]);
 
+    return (
+        <div className="container">
+            <div className="container-form">
+                <form onSubmit={handleSignIn}>
+                    <h1>Login</h1>
+                    <p>Please sign in to continue.</p>
 
-
-   function validationLogin(data){
-        const error ={}
-
-        const emailPattern= /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-        const passwordPattern= /^[a-zA-Z0-9!@#\$%\^\&*_=+-]{8,12}$/g;
-
-        if(data.email === ""){
-            error.email ="* Email is Required"
-        }
-        else if(!emailPattern.test(data.email)){
-            error.email="* Email did not match"
-        }
-
-        
-        if(data.password === ""){
-            error.password = "* Password is Required"
-        }
-        else if(!passwordPattern.test(data.password)){
-            error.password="* Password not valid"
-        }
-        
-        return error
-   }
-
-
-
-  return (
-    <div className="container">
-        <div className="container-form">
-            <form onSubmit={handleSignUp}>
-                <h1>Login</h1>
-                <p>Please sign in to continue.</p>
-                <div className="inputBox">
-                    <FiMail className='mail'/>
-                    <input type="email" 
-                            name="email" 
-                            id="email" 
+                    <div className="inputBox">
+                        <FiMail className='mail' />
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
                             onChange={handleChange}
-                            placeholder='Email'/> 
-                </div>
-                {error.email && <span style={{color:"red",display:"block",marginTop:"5px"}}>{error.email}</span>}
+                            placeholder='Email'
+                        />
+                    </div>
+                    {error.email && <span style={{ color: "red", display: "block", marginTop: "5px" }}>{error.email}</span>}
 
-                <div className="inputBox">
-                    <RiLockPasswordLine className='password'/>
-                    <input type="password" 
-                            name="password" 
-                            id="password" 
+                    <div className="inputBox">
+                        <RiLockPasswordLine className='password' />
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
                             onChange={handleChange}
-                            placeholder='Password'/>
-                </div>
-                {error.password && <span style={{color:"red",display:"block",marginTop:"5px"}}>{error.password}</span>}
-               
+                            placeholder='Password'
+                        />
+                    </div>
+                    {error.password && <span style={{ color: "red", display: "block", marginTop: "5px" }}>{error.password}</span>}
 
-                <div className='divBtn'>
-                    <small className='FG'>Forgot Password?</small>
-                    <button type='submit' className='loginBtn'>LOGIN</button>
-                </div>
-                
-            </form>
+                    <div className='divBtn'>
+                        <small className='FG'>Forgot Password?</small>
+                        <button type='submit' className='loginBtn'>LOGIN</button>
+                    </div>
+                </form>
 
-            <div className='dont'>
-                <p>Don't have an account? <Link to="/signup"><span>Sign up</span></Link></p>
+                <div className='dont'>
+                    <p>Don't have an account? <Link to="/signup"><span>Sign up</span></Link></p>
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Login
+export default Login;
+
